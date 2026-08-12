c36ad6994beedf0995b22148813c9859e919fd5e
SomeTechyGuy <cjhacks@duck.com>
2026-08-12 12:50:42 -0500

    attempting to undo whatever broke everything, and probably breaking this repo even more

--- release/src-rt-6.x.4708/router/wlconf_arm_714/wlconf.c
+++ release/src-rt-6.x.4708/router/wlconf_arm_714/wlconf.c
@@ -129,7 +129,6 @@
 		fprintf(stderr, "%s:%d:(%s): setting bsscfg #%d iovar \"%s\" " \
 				"to val 0x%x failed, err = %d\n",	\
 		        __FUNCTION__, __LINE__, ifname, bssidx, iovar, (unsigned int)val, ret);
-#define DHD_BSSIOVAR_SETINT(ifname, iovar, bssidx, val) do {} while(0)
 #endif
 #else
 #define WLCONF_DBG(fmt, arg...)
